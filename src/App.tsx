837ff3ffea31732be50d1717cd34093c167addcb
Nathan Palatin <nath.palatin@gmail.com>
2024-10-23 11:14:13 -0300

    :tada: first commit.

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+	return (
+		<div className="h-screen w-screen bg-zinc-950 flex justify-center items-center">
+			<h1 className="text-white text-lg">Hello world.</h1>
+		</div>
+	)
+}
